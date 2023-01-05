9ebb2a5b5a7566f67ab4f0aab8f449ff95b2e5df
miketheraptor <michael.j.acosta@gmail.com>
2023-01-04 19:55:53 -0500

    Continued work on codebase
    Mostly tweaked objects in models.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from models import *
+from engine import *
 
 
 pygame.init()
@@ -6,4 +8,17 @@ bounds = (1024, 768)
 window = pygame.display.set_mode(bounds)
 pygame.display.set_caption('Triple Pyad')
 
+def render_game(window):
+    window.fill((255, 255, 255))
+
+# game_engine = TriplePyadEngine()
+
+run = True
+while run:
+    render_game(window)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.update()
+pygame.quit()
 
--- models.py
+++ models.py
@@ -7,6 +7,51 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %I:%M',
     level=logging.INFO)
 
+
+
+class GameBoard:
+    '''
+    Creates a GameBoard object.
+    GameBoard grid position keys are as follows:
+            [0][0] [0][1] [0][2]
+            [1][0] [1][1] [1][2]
+            [2][0] [2][1] [2][2]
+    '''
+    grid = None
+
+    def __init__(self):
+
+        self.grid = [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None]
+        ]
+
+    def add(self, card, x_pos, y_pos):
+        '''
+        Add card to position
+        '''
+        self.grid[y_pos][x_pos] = card
+        logging.info(f'Placed {card.name} on {y_pos}, {x_pos}')
+
+
+class Player:
+    '''
+    Creates a Player object and associated
+    deck and hand objects.
+    '''
+    deck = None
+    hand = None
+    type = None # Player or Computer
+
+    def __init__(self, type):
+        self.deck = Deck(type) # Pass 'type' to Deck object so it knows its owner
+        self.hand = Hand()
+        self.type = type # Player or Computer
+
+    def play(self):
+        pass
+
 class Card:
     '''
     Creates a Card object with name and ranks.
@@ -18,33 +63,38 @@ class Card:
     rank_s = None
     rank_w = None
 
-    def __init__(self, name, *args):
+    def __init__(self, name, type, *args):
         ranks = args[0]
 
         self.name = name
+        self.type = type # Who owns card - Player or Computer
         self.rank_n = ranks[0]
         self.rank_e = ranks[1]
         self.rank_s = ranks[2]
         self.rank_w = ranks[3]
 
-        logging.info(f'Created card: {self.name}, {self.rank_n}, {self.rank_e}, {self.rank_s}, {self.rank_w}')
+        logging.info(f'For {self.type}, created card: {self.name}, {self.rank_n}, {self.rank_e}, {self.rank_s}, {self.rank_w}')
 
 class Deck:
     '''
     Creates a Deck object from a Deck CSV file.
-    CSV format is: name,rank_n,rank_e,_rank_s,rank_w
+    CSV syntax is: name,rank_n,rank_e,_rank_s,rank_w
     Each line in the CSV is converted to a Card object.
     Each Card object is added to the Deck object.
     '''
     cards = None
+    owner = None
 
-    def __init__(self, deck_file):
-        logging.info('Created deck')
+    def __init__(self, type):
         self.cards = []
+        self.type = type # Player or Computer
+
+    def set(self, deck_file):
         with open(deck_file) as f:
             reader = csv.DictReader(f)
             for row in reader:
-                self.cards.append(Card(row['name'], (row['rank_n'], row['rank_e'], row['rank_s'], row['rank_w'])))
+                self.cards.append(Card(row['name'], self.type, (row['rank_n'], row['rank_e'], row['rank_s'], row['rank_w'])))
+
 
 class Hand:
     '''
@@ -58,35 +108,49 @@ class Hand:
 
     def add(self, card):
         self.cards.append(card)
-        logging.info(f'Added {card} to hand')
+        logging.info(f'Added {card.name} to hand')
 
     def length(self):
         return len(self.cards)
 
-class GameBoard:
-    '''
-    Creates a GameBoard object.
-    GameBoard coordinate layout is as follows:
-            [0, 0] [1, 0] [2, 0]
-            [0, 1] [1, 1] [2, 1]
-            [0, 2] [1, 2] [2, 2]
-    '''
-
-    def __init__(self):
-        pass
 
 
 if __name__ == '__main__':
+    # models.py was run directly, therefor:
     logging.info('Debug mode active')
 
-    player_deck = Deck('test_deck.csv')
-    computer_deck = Deck('test_deck.csv')
-    player_hand = Hand()
-    # Adds cards from the player deck to the player hand.
+
+    # === Player Initialization ===
+
+    # Create the Player object
+    player = Player('Player')
+
+    # Set the Player object's deck
+    player.deck.set('test_deck.csv')
+
+    # Fill the Player object's hand with 5 cards from their deck
+    # In the game itself, this should be done by player action
     i = 0
     while i != 5:
-        player_hand.add(player_deck.cards[i])
-        print(player_hand.cards[i].name)
+        player.hand.add(player.deck.cards[i])
         i += 1
 
+    # === Computer Initialization ===
 
+    # Repeat above steps for Computer player
+    computer = Player('Computer')
+    computer.deck.set('test_deck.csv')
+    i = 0
+    while i != 5:
+        computer.hand.add(computer.deck.cards[i])
+        i += 1
+
+    # === GameBoard Initialization ===
+
+    # Create a blank GameBoard object
+    board = GameBoard()
+    print(board.grid[0][0])
+
+    # Place a card on the GameBoard grid
+    board.add(computer.hand.cards[0], 0, 0)
+    print(board.grid[0][0].name)
